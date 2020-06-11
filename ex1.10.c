84223f82893585c16f5afed1d5dd06c00a0b0f02
darrendollery <42651876+darrendollery@users.noreply.github.com>
2020-06-11 16:15:03 +0100

    Update ex1.10.c

--- ex1.10.c
+++ ex1.10.c
@@ -1,4 +1,8 @@
-/*Solution to K&R EX1.10 */
+/*Solution to K&R EX1.10
+
+Was very tempted to use PRINTF for the substituted characters as I had some difficulty getting it to compile using PUTCHAR.
+Finally realised this was because I was effectively trying to print both characters (backslash and letter) in a single
+PUTCHAR. Once I split each character across a seperate PUTCHAR statement it compiled and worked*/
 
 #include <stdio.h>
 

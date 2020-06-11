e7af2b3a018db9c2aedf576df06582c6385937a2
darrendollery <42651876+darrendollery@users.noreply.github.com>
2020-06-11 16:11:30 +0100

    Add files via upload

---
+++ ex1.10.c
@@ -0,0 +1,23 @@
+/*Solution to K&R EX1.10 */
+
+#include <stdio.h>
+
+int main (void ) {
+  char c;
+
+  while ((c = getchar())!=EOF) {
+    if (c == '\t'){
+      putchar('\\');
+      putchar('t');}
+    if (c == '\b') {
+      putchar('\\');
+      putchar('b');}
+    if (c == '\\') {
+      putchar('\\');
+      putchar('\\');}
+    else putchar(c);
+  }
+
+  return 0;
+}
+

7e993c4200e51a2df5e4d6b0c54c27d75325c2db
darrendollery <42651876+darrendollery@users.noreply.github.com>
2020-06-16 17:47:20 +0100

    Add files via upload

---
+++ ex1.13.vert.c
@@ -0,0 +1,43 @@
+/* A program to produce a histogram of the length of
+   word entered by the user. This is the 'vertical print' version */
+
+#include <stdio.h>
+
+int main (void)
+{
+  int c, state, count, i, line;
+  int wordlen[20];
+
+  state = count = line = 0;
+  for (i = 0; i < 20; ++i) {
+    wordlen[i] = 0;}
+
+  printf("Please enter a sentence :\n\n");
+
+  while ((c = getchar()) != EOF) {
+    if (c != ' ' && c != '\t' && c != '\n') {
+      ++count;} //If character entered is not 'blank' then increase word length counter
+    else {
+      ++wordlen[count-1]; //If character is blank, increase word length array entry by 1
+      count = 0;} //reset word length counter
+  }
+
+  printf("\nI will now print a histogram showing the lengths of the words you entered :\n\n");
+
+  for (line = 10; line > 0; --line) {
+    for (i = 1; i < 20; ++i) {
+      if (wordlen[i-1] >= line)
+	printf("  *");
+      else if (wordlen[i-1] < line)
+	printf("   ");
+    }
+    printf("\n");
+  }
+  for (i = 1; i < 20; ++i)
+    printf("%3d", i);
+
+  printf("\n");
+
+  return 0;
+}
+    
